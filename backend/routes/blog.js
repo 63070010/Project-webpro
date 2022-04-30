7d35ee737e4e758eb9a73201ae30b1616ea8851d
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2022-04-30 11:09:29 +0700

    Add files via upload

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path")
 const pool = require("../config");
-
+const { isLoggedIn } = require('../middlewares')
 router = express.Router();
 
 
@@ -27,54 +27,28 @@ router.get("/DetailsPromotion/:id", async function (req, res, next) {
 
 
 // โปรไฟล์
-router.get("/Profile_user/:id", async function (req, res, next) {
-  const promise1 = await pool.query("SELECT user_name, lname, fname, email, sex, Phonenumber, bank_name, bank_number, imageProflie FROM user left outer join author on(user.user_id = author.user_id) where user.user_id = ?", [
-    req.params.id,
-  ]);
-  const promise2 = await pool.query("SELECT * FROM book join cart_item  using(book_id) join cart using(cart_id) join payment using(cart_id) join author on(book.user_id = author.user_id) where cart.user_id = ? And cart.cart_id = payment.cart_id", [
-    req.params.id,
-  ]);
-  const promise3 = await pool.query("SELECT * FROM author join book on(author.user_id = book.user_id) where author.user_id = ?", [
-    req.params.id,
-  ]);
-  Promise.all([promise1, promise2, promise3])
-    .then((results) => {
-
-      const [myProfile, blogFields] = results[0];
-      const [mybook, commentFields] = results[1];
-      const [mysell_book, imageFields] = results[2];
-      res.json({
-        Profile: myProfile[0],
-        book: mybook,
-        sell_book: mysell_book,
-        error: null,
-      });
-    })
-    .catch((err) => {
-      return res.status(500).json(err);
-    });
-});
 
 
 
 
 // เช็คตะกร้า
 
-router.get("/cart_check/:id", async function (req, res, next) {
+router.get("/cart_check",isLoggedIn, async function (req, res, next) {
   const promise4 = await pool.query("SELECT * FROM cart where user_id = ?", [
-    req.params.id,
+    req.user.id,
   ]);
   const promise6 = await pool.query("SELECT * FROM cart join payment using(cart_id) where user_id = ?", [
-    req.params.id,
+    req.user.id,
   ]);
   const promise7 = await pool.query("SELECT * FROM book join cart_item  on(book.id = cart_item.book_id) join cart using(cart_id) join payment using(cart_id) join author on(book.user_id = author.user_id) where cart.user_id = ? And cart.cart_id = payment.cart_id", [
-    req.params.id,
+    req.user.id,
   ]);
   Promise.all([promise4, promise6, promise7])
     .then((results) => {
       const [cart, imageFields] = results[0];
       const [payment, commentFields] = results[1];
       const [mybook, c] = results[2];
+      console.log(results[2])
       res.json({
         cart: cart,
         payment: payment,
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,65 +1,86 @@
+const { json } = require("express");
 const express = require("express");
-const router = express.Router();
 const pool = require("../config");
 
-// Get comment
+const router = express.Router();
 
-// cart
-
-
-router.post('/addcart/:id', async function (req, res, next) {
-    try {
-        const [rows1, fields1] = await pool.query(
-            'INSERT INTO `cart` (`create_date`, `total_price`, `user_id`, `promotion_id`) VALUES (CURRENT_TIMESTAMP, 0, ?, null )',
-            [req.params.id]
-        )
-        res.json(rows2)
-    } catch (err) {
-        console.log(err)
-    }
+router.get('/:blogId/comments', function (req, res, next) {
 });
-//// เพิ่มหนังสือในตะกร้า
-router.post('/addbook/:id', async function (req, res, next) {
+
+router.post('/:blogId/comments', async function (req, res, next) {
+    const comment = req.body.comment
+
+    const conn = await pool.getConnection()
+    await conn.beginTransaction();
+
     try {
-        const [rows1, fields1] = await pool.query(
-            'INSERT INTO `cart_item` (`book_id`, `price`, `cart_id`) VALUES (?, ?, ?)',
-            [req.params.id, req.body.price, req.body.cart_id]
+        const [rows1, fields1] = await conn.query(
+            'INSERT INTO `comments` (`blog_id`, `comment`, `like`, `comment_date`) VALUES (?, ?, ?, CURRENT_TIMESTAMP)',
+            [req.params.blogId, comment, 0]
         )
-        const [rows2, fields2] = await pool.query(
-            'select * from cart_item where item_no = ?',
+        const [rows2, fields2] = await conn.query(
+            'SELECT * FROM `comments` WHERE `id` = ?',
             [rows1.insertId]
         )
-        res.json(rows2)
+        await conn.commit()
+        return res.json(rows2[0])
     } catch (err) {
-        console.log(err)
+        await conn.rollback();
+        return res.status(500).json(err)
+    } finally {
+        console.log('finally')
+        conn.release();
     }
 });
 
-// Update comment
-router.put('/EditProfile/:idUser', async function (req, res, next) {
-    const [rows, fields1] = await pool.query(
-        'SELECT * FROM user WHERE id = ?',
-        [req.params.idUser]
-    )
-    res.send({
-        message: 'Comment ID ' + req.params.commentId + ' is updated.',
-        comment: rows2[0]
+router.put('/comments/:commentId', async function (req, res, next) {
+    try {
+        const [rows1, fields1] = await pool.query(
+            'UPDATE comments SET comment=? WHERE id=?', [req.body.comment, req.params.commentId]
+        )
+        console.log(rows1)
+        res.json({ comment: req.body.comment })
+    } catch (error) {
+        res.status(500).json(error)
     }
-    )
-
-    //affectedRows
-    //selectRow
-});
-
-
-// Delete comment
-router.delete('/comments/:commentId', function (req, res, next) {
-    return
 });
 
 // Delete comment
-router.put('/comments/addlike/:commentId', function (req, res, next) {
-    return
+router.delete('/comments/:commentId', async function (req, res, next) {
+    try {
+        const [rows1, fields1] = await pool.query(
+            'DELETE FROM comments WHERE id=?', [req.params.commentId]
+        )
+        res.json("success")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+// Add Like comment
+router.put('/comments/addlike/:commentId', async function (req, res, next) {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction();
+
+    // note : like is a syntax of sql. must call name of table before like
+    try {
+        const [rows1, fields1] = await conn.query(
+            'SELECT * FROM comments WHERE id=?', [req.params.commentId]
+        )
+        await conn.query(
+            `UPDATE comments SET comments.like = ? WHERE id=?`, [rows1[0].like + 1, req.params.commentId]
+        )
+
+        await conn.commit()
+
+        res.json({ message: "Like Incress", like: rows1[0].like + 1 })
+    } catch (error) {
+        await conn.rollback();
+        res.status(500).json(error)
+    } finally {
+        console.log('finally')
+        conn.release();
+    }
 });
 
 
